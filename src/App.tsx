bec1c16ddedcb2c3f596f9889fbfd0087b8bbcb1
Shoumik Sarkar <110356808+ssavi45@users.noreply.github.com>
2025-08-02 14:10:24 +0000

    packages added

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Quiz from './components/Quiz';
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Football Genius</h1>
+      <Quiz />
+    </div>
+  );
+}
+
+export default App;
